bdb39722435f6094e12ffbed715934cf954b7ea1
Dustin Clark <138034989+Dclark2434@users.noreply.github.com>
2025-03-24 19:30:12 -0500

    Add player shooting cooldown mechanism to limit shot frequency

--- player.py
+++ player.py
@@ -4,9 +4,11 @@ from constants import *
 from shot import *
 
 class Player(CircleShape):
+    
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.rate_limit = 0
     
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -23,6 +25,7 @@ class Player(CircleShape):
         self.rotation += (PLAYER_TURN_SPEED * dt)
 
     def update(self, dt):
+        self.rate_limit -= dt
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
@@ -41,5 +44,10 @@ class Player(CircleShape):
         self.position += forward * PLAYER_SPEED * dt
 
     def shoot(self):
+        if self.rate_limit > 0:
+            return
         shot = Shot(self.position.x, self.position.y)
         shot.velocity = pygame.Vector2(0,1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+        self.rate_limit = PLAYER_SHOOT_COOLDOWN
+
+    
